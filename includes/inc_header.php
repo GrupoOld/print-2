fb98386b8eda935cc3ed75efd5945e05096ae936
GrupoOld <fatec.gold@gmail.com>
2023-11-02 11:34:08 -0300

    Segundo Commit

---
+++ includes/inc_header.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<header>
+        <div class="container text-center">
+            <menu class="navbar navbar-expand-lg" style="background-color: azure;">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                        <img src="images/Logo Grupo OLD 50x50.png" alt="Logo GOLD" width="50" height="50">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">
+                                    História
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">História Geral</a></li>
+                                    <li><a class="dropdown-item" href="#">Capela Santo Antônio</a></li>
+                                    <li><a class="dropdown-item" href="#">Brasital</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">
+                                    Turismo
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Pontos Turísticos</a></li>
+                                    <li><a class="dropdown-item" href="#">Roteiro do Vinho</a></li>
+                                    <li><a class="dropdown-item" href="#">Castelo de São Roque</a></li>
+                                    <li><a class="dropdown-item" href="#">Catarina Outlet</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Serviços</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">
+                                    Notícias
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Home - Notícias</a></li>
+                                    <li><a class="dropdown-item" href="#">Notícias da Cidade</a></li>
+                                    <li><a class="dropdown-item" href="#">Notícias da Cultura</a></li>
+                                    <li><a class="dropdown-item" href="#">Notícias da Educação</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">
+                                    Cultura e Lazer
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Home - Cultura e Lazer</a></li>
+                                    <li><a class="dropdown-item" href="#">Cultura</a></li>
+                                    <li><a class="dropdown-item" href="#">Lazer</a></li>
+                                    <li><a class="dropdown-item" href="#">Agenda</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Quem Somos</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Contato</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Termos de Uso</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </menu>
+            <div class="row">
+                <h1>CONHEÇA SÃO ROQUE</h1>
+            </div>
+        </div>
+        </div>
+    </header>
+</body>
+</html>
