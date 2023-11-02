0759b02655236f7afe13f2827072ce00ae081b4f
joel-t-junior <joel.teixeira01@fatec.sp.gov.br>
2023-11-02 12:33:19 -0300

    Adicionei minha página

---
+++ paginas/culturaelazer.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Menu no Bootstrap</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-
+T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/meu.css">
+</head>
+
+<body>
+    <?php include_once 'includes/inc_header.php'; ?>
+    <article>
+        <div class="container text-center">
+            <div class="row-fluid">
+                <div class="col-sm-12 col-md-12 col-lg-12">
+                    <br>
+                    <img src="https://placekitten.com/800/200" class="img-fluid" alt="Logotipo da empresa de nome Gold">
+                </div>
+            </div>
+        </div>
+        <div class="container text-center">
+            <div class="col-sm-12 col-md-12 col-lg-12"></div>
+            <h3><br>HISTÓRIA DE SÃO ROQUE</h3><br>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-md-6 col-lg-6">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque sed ut perspiciatis beatae iure
+                        architecto praesentium deserunt optio? Repellat laborum incidunt est similique animi
+                        reprehenderit earum, architecto nobis eos recusandae!</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate rerum labore repellendus rem
+                        asperiores cum quisquam, dolorem culpa debitis a quis vero, odio, ullam error quae. Officia modi
+                        excepturi doloribus.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, sint repudiandae modi sed ipsum
+                        esse, officiis temporibus incidunt molestias impedit sit ea labore, excepturi similique debitis
+                        aliquam autem repellat adipisci.</p>
+                    <img src="https://placekitten.com/400/200" class="img-fluid" alt="Logotipo da empresa de nome Gold">
+                </div>
+                <div class="col-md-6 col-lg-6">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis possimus ducimus esse quia
+                        pariatur, non iste voluptatem laborum alias deleniti repellendus eveniet voluptatibus velit. Nam
+                        ea quam alias dolor unde.</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto laborum officia dolor
+                        consequuntur quod aliquid mollitia ipsum facilis neque tempora, tenetur numquam, qui cumque
+                        voluptas perspiciatis minus ipsa. Quaerat, non!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus beatae expedita laborum,
+                        adipisci cumque illum? Deleniti, provident? Ex, consequuntur eos? Tempore fuga ea ipsum iste
+                        accusantium placeat cum possimus aperiam.</p>
+                    <img src="https://placekitten.com/405/200" class="img-fluid" alt="Logotipo da empresa de nome Gold">
+
+                </div>
+            </div>
+        </div>
+    </article>
+    <footer class="text-center">
+        <br><small>Copystart Joel Teixeira Junior - RA: 2650832313001 - Todos os Direitos estão Reservados.</small>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-
+C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
