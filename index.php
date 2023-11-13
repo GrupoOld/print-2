ca54930e1f49e415759178b5a7be1df7623caef7
guilherme-filipini <139499115+Poser404@users.noreply.github.com>
2023-11-13 19:42:06 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -13,6 +13,12 @@ T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="a
 <body>
     <?php include_once 'includes/inc_header.php'; ?>
     <article>
+        
+        </div>
+        <div class="container text-center">
+            <div class="col-sm-12 col-md-12 col-lg-12"></div>
+            <h3 class= "h3-historia">HISTÓRIA DE SÃO ROQUE</h3>
+        </div>
         <div class="container text-center">
             <div class="row-fluid">
                 <div class="col-sm-12 col-md-12 col-lg-12">
@@ -21,12 +27,9 @@ T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="a
                 </div>
             </div>
         </div>
-        <div class="container text-center">
-            <div class="col-sm-12 col-md-12 col-lg-12"></div>
-            <h3><br>HISTÓRIA DE SÃO ROQUE</h3><br>
-        </div>
         <div class="container">
             <div class="row">
+                
                 <div class="col-sm-12 col-md-6 col-lg-6">
                     <p>No final do século XVII, a cidade de São Roque foi estabelecida por Pedro Vaz de Barros, mais conhecido como Vaz-Guaçu, em virtude de sua devoção a São Roque. Ele se mudou para a região com sua família e cerca de 1.200 índios, às margens dos ribeirões Carambeí e Aracaí, começando a cultivar trigo e uva. A cidade se originou de uma vasta fazenda e uma capela dedicada a São Roque, situada onde hoje está a Praça da Matriz.</p>
                     <p>A princípio, a fazenda trabalhava com mão-de-obra indígena e, mais tarde, escravos negros para cultivar vinhas e campos de trigo. Posteriormente, os imigrantes italianos e portugueses plantaram vinhedos nas encostas das colinas e tornaram São Roque a famosa "Terra do Vinho".</p>
