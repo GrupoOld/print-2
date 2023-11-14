018c0e4cc19fca0d3f79931d2774efa26438102a
guilherme-filipini <139499115+Poser404@users.noreply.github.com>
2023-11-14 18:48:12 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -11,44 +11,49 @@ T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="a
 </head>
 
 <body>
-    <?php include_once 'includes/inc_header.php'; ?>
-    <article>
-        
-        </div>
-        <div class="container text-center">
-            <div class="col-sm-12 col-md-12 col-lg-12"></div>
-            <h3 class= "h3-historia">HISTÓRIA DE SÃO ROQUE</h3>
-        </div>
-        <div class="container text-center">
-            <div class="row-fluid">
-                <div class="col-sm-12 col-md-12 col-lg-12">
-                    <br>
-                    <img src="https://placekitten.com/800/200" class="img-fluid" alt="Logotipo da empresa de nome Gold">
-                </div>
-            </div>
-        </div>
-        <div class="container">
+<?php include_once 'includes/inc_header.php'; ?>
+    <div class="container text-center">
             <div class="row">
-                
                 <div class="col-sm-12 col-md-6 col-lg-6">
-                    <p>No final do século XVII, a cidade de São Roque foi estabelecida por Pedro Vaz de Barros, mais conhecido como Vaz-Guaçu, em virtude de sua devoção a São Roque. Ele se mudou para a região com sua família e cerca de 1.200 índios, às margens dos ribeirões Carambeí e Aracaí, começando a cultivar trigo e uva. A cidade se originou de uma vasta fazenda e uma capela dedicada a São Roque, situada onde hoje está a Praça da Matriz.</p>
-                    <p>A princípio, a fazenda trabalhava com mão-de-obra indígena e, mais tarde, escravos negros para cultivar vinhas e campos de trigo. Posteriormente, os imigrantes italianos e portugueses plantaram vinhedos nas encostas das colinas e tornaram São Roque a famosa "Terra do Vinho".</p>
-                    <p>Em 1764, o local foi designado como freguesia de Santana de Parnaíba antes de se tornar uma vila própria. Somente em 1864 São Roque foi declarado um município. Entre 1872 e 1875, ocorreram eventos significativos na cidade. Entre eles estavam a inauguração da Santa Casa de Misericórdia e a construção da estação da Estrada de Ferro Sorocabana. A chegada de imigrantes italianos no final do século XIX impulsionou a economia da área e impulsionou o crescimento de São Roque.</p>
-                    <p>O I Cartório de Protesto de Notas e Títulos foi fundado em São Roque em 1834. O fórum judicial foi criado com dois ofícios judiciais em 1873, e o 1° Cartório de Registro de Imóveis foi estabelecido na cidade em 1874. O Primeiro Ofício do Registro Civil das Pessoas Naturais foi inaugurado em 1889, e o 2° Cartório de Protesto de Notas e Títulos começou a funcionar na área em 1893.</p>
-                    <img src="https://placekitten.com/400/200" class="img-fluid" alt="Logotipo da empresa de nome Gold">
+                    <section class = "padding">
+                        <img src="https://placekitten.com/303/313" alt="Imagem Via Placekitten">
+                    </section>
                 </div>
-                <div class="col-md-6 col-lg-6">
-                    
-                    <p>A realização dessas melhorias foi principalmente atribuída à influência política do Sr. Antônio Joaquim da Rosa, também conhecido como o Barão de Piratininga. O Barão de Piratininga era uma figura importante em São Roque e aparentemente era um amigo do Imperador D. Pedro II. Em 1869, o Barão de Piratininga foi nomeado presidente da província de São Paulo.</p>
-                    <p>A BRASITAL, uma das primeiras indústrias têxteis do Brasil, foi fundada por o industrial italiano Enrico Dell'Acqua em 1890 e continuou a funcionar até meados dos anos 1970. Um Centro Cultural, Educativo e Turístico, além da biblioteca municipal, agora faz parte do patrimônio público da cidade.</p>
-                    <p>Três pioneiros - o lavrador José Casali, o engenheiro da Estrada de Ferro Sorocabana Dr. Eusébio Stevaux (francês) e Antônio dos Santos Sobrinho, também conhecido como Santinhos e nativo de São Roque - tentaram revitalizar a vitivinicultura em São Roque a partir de 1880. Embora apenas José Casali tenha se dedicado à vinicultura de forma comercial, todos os outros tinham seguidores. Embora as condições da região fossem ideais para o cultivo de vinhas, as técnicas de vinicultura eram obsoletas e careciam de apoio do governo. Do final do século XIX até a primeira década do século XX, houve um desenvolvimento gradual no cultivo da uva e na produção de vinho.</p>
-                    <p>Os irmãos Boccato fundaram a primeira tipografia de São Roque e começaram a publicar um semanário chamado "O Democrata". Este jornal foi lançado pela primeira vez em 1917. Em 1947, o primeiro ginásio da cidade foi inaugurado, a escola "Horácio Manley Lane".</p>
-                    <img src="https://placekitten.com/405/200" class="img-fluid" alt="Logotipo da empresa de nome Gold">
-
+                <div class="col-sm-12 col-md-6 col-lg-6">
+                <section class = "padding">
+                        <img src="https://placekitten.com/303/313" alt="Imagem Via Placekitten">
+                    </section>
+                </div>
+                <div class="col-sm-12 col-md-6 col-lg-6">
+                    <section class = "padding">
+                        <img src="https://placekitten.com/303/313" alt="Imagem Via Placekitten">
+                    </section>
+                </div>
+                <div class="col-sm-12 col-md-6 col-lg-6">
+                <section class = "padding">
+                        <img src="https://placekitten.com/303/313" alt="Imagem Via Placekitten">
+                    </section>
+                </div>
+                <div class="col-sm-12 col-md-6 col-lg-6">
+                    <section class = "padding">
+                        <img src="https://placekitten.com/303/313" alt="Imagem Via Placekitten">
+                    </section>
+                </div>
+                <div class="col-sm-12 col-md-6 col-lg-6">
+                <section class = "padding">
+                        <img src="https://placekitten.com/303/313" alt="Imagem Via Placekitten">
+                    </section>
+                </div>
+                <div class="col-sm-12 col-md-12 col-lg-12">
+                <section class = "padding">
+                        <img src="https://placekitten.com/303/313" alt="Imagem Via Placekitten">
+                    </section>
+                </div>
+             
                 </div>
             </div>
         </div>
-    </article>
+
     <footer class="text-center">
         <br><small>Copystart Joel Teixeira Junior - RA: 2650832313001 - Todos os Direitos estão Reservados.</small>
     </footer>
