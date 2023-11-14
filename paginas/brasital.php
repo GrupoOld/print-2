a4abf49bebc8f0897d51a92528ef56d44d448737
guilherme-filipini <139499115+Poser404@users.noreply.github.com>
2023-11-14 14:39:32 -0300

    Add files via upload

---
+++ paginas/brasital.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Menu no Bootstrap</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-
+T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/gui.css">
+</head>
+
+<body>
+    <?php include_once '../includes/inc_header.php'; ?>
+    <article>
+        <div class="container text-center">
+            <div class="col-sm-12 col-md-12 col-lg-12"></div>
+            <h3 class= "h3-historia">CAPELA DE ST. ANTÔNIO</h3>
+        <section>
+        <img src="https://placekitten.com/994/313" class="img-fluid" alt="Logotipo da empresa de nome Gold">
+        </section>
+        </div>
+        <div class="col-sm-12 col-md-12 col-lg-12">
+            <article class = "top-historia">
+                <p>A Brasital é mais do que apenas uma antiga fábrica; é um dos patrimônios mais importantes de São Roque devido ao efeito que teve na cultura e na economia do município. A Brasital, fundada por industrial italiano Enrico Dell'Acqua em 1890, era uma das indústrias têxteis mais inovadoras de São Paulo. O edifício da Brasital está entre os monumentos históricos mais importantes da área.</p>
+                <p>A indústria diminuiu gradualmente com o tempo, até que a Brasital fechou definitivamente em 1970. Depois de dezessete anos, em 1º de maio de 1987, o local foi reinaugurado com um novo nome: Brasital Center for Education and Culture. Ao mesmo tempo, o Governo Estadual ajudou a incorporar a infraestrutura da Brasital S/A ao patrimônio público de São Roque.</p>
+                <p>Os departamentos de Educação e Cultura da prefeitura estão atualmente sediados no edifício principal da Brasital. Além disso, o local abriga o Banco do Povo e o Posto de Atendimento do Sebrae, localizado na portaria da rua Rui Barbosa.</p>
+            </article>
+        </div>
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-sm-12 col-md-4 col-lg-4">
+                    <section>
+                        <img src="https://placekitten.com/303/313" alt="Imagem Via Placekitten">
+                    </section>
+                </div>
+                <div class="col-sm-12 col-md-4 col-lg-4">
+                <section>
+                        <img src="https://placekitten.com/303/313" alt="Imagem Via Placekitten">
+                    </section>
+                </div>
+                <div class="col-sm-12 col-md-4 col-lg-4">
+                <section>
+                        <img src="https://placekitten.com/303/313" alt="Imagem Via Placekitten">
+                    </section>
+                </div>
+            </div>
+        </div>
+            <div class="container text-center">
+                <div class="row">
+                <div class="col-md-4 col-lg-4">
+                    <section class = top-historia>
+                        <figure>
+                        <img src="https://placekitten.com/206/200" class="img-fluid" alt="Logotipo da empresa de nome Gold">
+                        <a href="../paginas/capela.php"><figcaption>Capela de St. Antônio</figcaption></a>
+                        </figure>
+                    </div>
+                    </section>
+                <div class="col-md-4 col-lg-4">
+                    <section class = top-historia>
+                    <figure>
+                        <img src="https://placekitten.com/206/200" class="img-fluid" alt="Logotipo da empresa de nome Gold">
+                        <a href="../paginas/historia_geral.php"><figcaption>História Geral</figcaption></a>
+                        </figure>
+                    </section>
+                </div>
+                    <div class="col-md-4 col-lg-4">
+                    <section class = top-historia>
+                    <figure>
+                        <img src="https://placekitten.com/206/200" class="img-fluid" alt="Logotipo da empresa de nome Gold">
+                        <a href="../paginas/brasital.php"><figcaption>BRASITAL</figcaption></a>
+                        </figure>
+                </section>
+            </div>
+            </div>
+        </div>
+    </article>
+    <footer class="text-center">
+        <br><small>Copystart Joel Teixeira Junior - RA: 2650832313001 - Todos os Direitos estão Reservados.</small>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-
+C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ paginas/capela.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Menu no Bootstrap</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-
+T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/gui.css">
+</head>
+
+<body>
+    <?php include_once '../includes/inc_header.php'; ?>
+    <article>
+        <div class="container text-center">
+            <div class="col-sm-12 col-md-12 col-lg-12"></div>
+            <h3 class= "h3-historia">CAPELA DE ST. ANTÔNIO</h3>
+        <section>
+        <img src="https://placekitten.com/994/313" class="img-fluid" alt="Logotipo da empresa de nome Gold">
+        </section>
+        </div>
+        <div class="col-sm-12 col-md-12 col-lg-12">
+            <article class = "top-historia">
+                <p>Em 1681, a Capela de Santo Antônio foi construída devido ao desejo persistente de ona Maria Mendonça, esposa do bandeirante da área. A capela, localizada perto da Casa Grande, foi construída principalmente com taipa de pilão, exceto pela torre, construída com pedra e revestida com barro. As capelas jesuíticas eram os padrões para sua construção, o que a distinguia das outras construções religiosas da época.</p>
+                <p>A mobília da capela é bastante simples, com exceção do altar-mor folheado a ouro e da pia batismal original que remonta à época em que foi construída. A capela desempenhou um papel importante como local para celebrar missas, apesar de ser pequena e simples.</p>
+            </article>
+        </div>
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-sm-12 col-md-4 col-lg-4">
+                    <section>
+                        <img src="https://placekitten.com/303/313" alt="Imagem Via Placekitten">
+                    </section>
+                </div>
+                <div class="col-sm-12 col-md-4 col-lg-4">
+                <section>
+                        <img src="https://placekitten.com/303/313" alt="Imagem Via Placekitten">
+                    </section>
+                </div>
+                <div class="col-sm-12 col-md-4 col-lg-4">
+                <section>
+                        <img src="https://placekitten.com/303/313" alt="Imagem Via Placekitten">
+                    </section>
+                </div>
+            </div>
+        </div>
+            <div class="container text-center">
+                <div class="row">
+                <div class="col-md-4 col-lg-4">
+                    <section class = top-historia>
+                        <figure>
+                        <img src="https://placekitten.com/206/200" class="img-fluid" alt="Logotipo da empresa de nome Gold">
+                        <a href="../paginas/capela.php"><figcaption>Capela de St. Antônio</figcaption></a>
+                        </figure>
+                    </div>
+                    </section>
+                <div class="col-md-4 col-lg-4">
+                    <section class = top-historia>
+                    <figure>
+                        <img src="https://placekitten.com/206/200" class="img-fluid" alt="Logotipo da empresa de nome Gold">
+                        <a href="../paginas/historia_geral.php"><figcaption>História Geral</figcaption></a>
+                        </figure>
+                    </section>
+                </div>
+                    <div class="col-md-4 col-lg-4">
+                    <section class = top-historia>
+                    <figure>
+                        <img src="https://placekitten.com/206/200" class="img-fluid" alt="Logotipo da empresa de nome Gold">
+                        <a href="../paginas/brasital.php"><figcaption>BRASITAL</figcaption></a>
+                        </figure>
+                </section>
+            </div>
+            </div>
+        </div>
+    </article>
+    <footer class="text-center">
+        <br><small>Copystart Joel Teixeira Junior - RA: 2650832313001 - Todos os Direitos estão Reservados.</small>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-
+C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
