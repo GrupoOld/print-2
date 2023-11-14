31cd54469bea26a6bc07dea2e21d1411dcf455a8
GleitonF <149203589+GleitonF@users.noreply.github.com>
2023-11-14 18:23:08 -0300

    pagina de noticias

---
+++ paginas/home_noticias.php
@@ -0,0 +1,135 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home noticias</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-
+T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="..\css\meu.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+</head>
+
+
+
+<body>
+
+    <?php include_once 'C:\xampp\htdocs\print_2\includes\inc_not1.php'; ?>
+    <?php include_once 'C:\xampp\htdocs\print_2\includes\inc_not2.php'; ?>
+
+
+    <main>
+        <div class="container text-center">
+            <div class="row-fluid"></div>
+        </div>
+        <div class="container rigth">
+            <div class="col-sm-12 col-md-12 col-lg-12"></div>
+            <h2><br>Notícias de São Roque</h2><br>
+            <h3><br>Tudo o que você precisa saber</h3><br>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-md-6 col-lg-6">
+                    <img src="https://placekitten.com/405/200" class="img-fluid" alt="Imagem de gatinho">
+                    <p>Aqui você acompanha de perto as notícias da nossa cidade.</p>
+                </div>
+                <div class="col-sm-12 col-md-6 col-lg-6">
+                    <img src="https://placekitten.com/405/200" class="img-fluid" alt="Imagem de gatinho">
+                    <p>Notícias da Cidade:
+                        Saiba como está nossa cidade.<br>
+                        Expectativa de crescimento nas vendas estimula investimentos para fim de ano
+                    </p>
+                </div>
+                <div class="col-sm-12 col-md-6 col-lg-6">
+                    <img src="https://placekitten.com/405/200" class="img-fluid" alt="Imagem de gatinho">
+                    <p>Notícias da Cultura:
+                        Divisão de Cultura divulga o Resultado de Avaliação de Mérito Cultural, dos Editais da Lei Paulo Gustavo
+                    </p>
+                </div>
+                <div class="col-md-6 col-lg-6">
+                    <img src="https://placekitten.com/405/200" class="img-fluid" alt="imagem de gatinho 2">
+                    <p>Notícias da Educação:
+                        Fatec São Roque abre inscrições para vestibular 1° semestre 2024.
+                    </p>
+
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <article>
+        <div class="container text-center">
+            <div class="row">
+
+                <section class="col-12 col-sm-12 col-md-4 col-lg-4">
+                    
+                    <img src="https://placekitten.com/96/139" alt="Notícias da Cidade">
+                    <p><a href="..paginas/noticias2.php"><h5>Notícias da Cidade</h5></a></p>
+                </section>
+
+                <section class="col-12 col-sm-12 col-md-4 col-lg-4">
+                                        <img src="https://placekitten.com/96/139" alt="Notícias da Cultura">
+                    <p><a href="C:\xampp\htdocs\print_2\paginas\noticias3.php"><h5> Notícias da Cultura</h5></a></p>
+                </section>
+
+                <section class="col-12 col-sm-12 col-md-4 col-lg-4">
+                                       <img src="https://placekitten.com/96/139" alt="Notícias da Educação">
+                    <p><a href="paginas/noticias4.php"><h5>Notícias da Educação</h5></a></p>
+                </section>
+
+            </div>
+        </div>
+    </article>
+
+</body>
+
+
+<footer class="text-center">
+
+
+    <div class="container">
+        <div class="row">
+
+            <section class="col-1 col-md-1 align-self-start">
+                <h1><a href="www.instagram.com.br"><span class="bi bi-instagram"></span></a></h1>
+            </section>
+            <section class="col-1 col-md-1 align-self-start">
+                <h1><a href="www.youtube.com.br"><span class="bi bi-youtube"></span></a></h1>
+            </section>
+            <section class="col-1 col-md-1 align-self-star">
+                <h1><a href="www.twitter.com.br"><span class="bi bi-twitter"></span></a></h1>
+            </section>
+            <section class="col-1 col-md-1 align-self-start">
+                <h1><a href="www.facebook.com.br"><span class="bi bi-facebook"></span></a></h1>
+            </section>
+
+            <div class="col-5 nomes">
+                <small>Todos Direitos Reservados:</small>
+                <br><small>Gleiton Correia Fernandes - RA: 2650832313036 </small>
+                <br><small>Guilherme Filipini - RA: 2650832313008 -</small>
+                <br><small>Joel Teixeira Jr - RA: 2650832313001 </small>
+                <br><small>Lucia Midori Nishimori - RA: 2650832313035</small>
+            </div>
+
+
+            <div class="col-3 logof">
+                <a class="navbar-brand" href="#">
+                    <img src="../images/logo_cffa2.svg" alt="Logo GOLD" width="100" height="100">
+                </a>
+            </div>
+        </div>
+    </div>
+
+</footer>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-
+        C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.7/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+</html>
