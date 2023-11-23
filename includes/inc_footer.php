e40909e9f7a22c31a69653d2079fd12cad1bc4d6
guilherme-filipini <139499115+Poser404@users.noreply.github.com>
2023-11-23 20:13:35 -0300

    Add files via upload

---
+++ includes/inc_footer.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Noticias 3</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-
+T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="..\css\meu.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+</head>
+
+
+<footer class="text-center">
+
+
+    <div class="container">
+        <div class="row">
+
+            <section class="col-1 col-md-1 align-self-start">
+                <h1><a href="www.instagram.com.br"><span class="bi bi-instagram"></span></a></h1>
+            </section>
+            <section class="col-1 col-md-1 align-self-start">
+                <h1><a href="www.youtube.com.br"><span class="bi bi-youtube"></span></a></h1>
+            </section>
+            <section class="col-1 col-md-1 align-self-star">
+                <h1><a href="www.twitter.com.br"><span class="bi bi-twitter"></span></a></h1>
+            </section>
+            <section class="col-1 col-md-1 align-self-start">
+                <h1><a href="www.facebook.com.br"><span class="bi bi-facebook"></span></a></h1>
+            </section>
+
+            <div class="col-5 nomes">
+                <small>Todos Direitos Reservados:</small>
+                <br><small>Gleiton Correia Fernandes - RA: 2650832313036 </small>
+                <br><small>Guilherme Filipini - RA: 2650832313008 -</small>
+                <br><small>Joel Teixeira Jr - RA: 2650832313001 </small>
+                <br><small>Lucia Midori Nishimori - RA: 2650832313035</small>
+            </div>
+
+
+            <div class="col-3 logof">
+                <a class="navbar-brand" href="C:\xampp\htdocs\print-2\index.php">
+                    <img src="../images/logo_cffa2.svg" alt="Logo GOLD" width="100" height="100">
+                </a>
+            </div>
+        </div>
+    </div>
+
+</footer>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-
+        C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.7/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+</html>
