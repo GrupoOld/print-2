1f544c14c57424bdf14765cf39b2a4ccfd35814d
guilherme-filipini <139499115+Poser404@users.noreply.github.com>
2023-11-27 19:25:37 -0300

    Add files via upload

--- paginas/historia_geral.php
+++ paginas/historia_geral.php
@@ -91,9 +91,8 @@ T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="a
             </div>
         </div>
     </article>
-    <footer class="text-center">
-        <br><small>Copystart Joel Teixeira Junior - RA: 2650832313001 - Todos os Direitos estão Reservados.</small>
-    </footer>
+    
+    <?php include_once '../includes/inc_footer.php'; ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-
 C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
