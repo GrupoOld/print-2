01f4a4394884828ac681483358fcbe3eb140acf3
guilherme-filipini <139499115+Poser404@users.noreply.github.com>
2023-11-28 14:47:09 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -11,42 +11,71 @@ T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="a
 </head>
 
 <body>
-<?php include_once 'includes/inc_header.php'; ?>
+<?php include_once 'includes/inc_header_index.php'; ?>
     <div class="container text-center">
             <div class="row">
                 <div class="col-sm-12 col-md-6 col-lg-6">
                     <section class = "padding">
+                        <figure>
                         <img src="https://placekitten.com/303/313" alt="Imagem Via Placekitten">
+                        <figcaption><a href="paginas/historia_geral.php">História</a></figcaption>
+                        </figure>
                     </section>
                 </div>
                 <div class="col-sm-12 col-md-6 col-lg-6">
                 <section class = "padding">
+                        <figure>
                         <img src="https://placekitten.com/303/313" alt="Imagem Via Placekitten">
+                        <figcaption><a href="paginas/pontosturisticos.php">Turismo</a></figcaption>
+                        </figure>
                     </section>
                 </div>
                 <div class="col-sm-12 col-md-6 col-lg-6">
                     <section class = "padding">
+                    <figure>
                         <img src="https://placekitten.com/303/313" alt="Imagem Via Placekitten">
+                        <figcaption><a href="#">Serviços</a></figcaption>
+                        </figure>
                     </section>
                 </div>
                 <div class="col-sm-12 col-md-6 col-lg-6">
                 <section class = "padding">
+                        <figure>
                         <img src="https://placekitten.com/303/313" alt="Imagem Via Placekitten">
+                        <figcaption><a href="paginas/home_noticias.php">Notícias</a></figcaption>
+                        </figure>
                     </section>
                 </div>
                 <div class="col-sm-12 col-md-6 col-lg-6">
                     <section class = "padding">
+                    <figure>
                         <img src="https://placekitten.com/303/313" alt="Imagem Via Placekitten">
+                        <figcaption><a href="paginas/culturaelazer.php">Cultura & Lazer</a></figcaption>
+                        </figure>
                     </section>
                 </div>
                 <div class="col-sm-12 col-md-6 col-lg-6">
                 <section class = "padding">
+                        <figure>
                         <img src="https://placekitten.com/303/313" alt="Imagem Via Placekitten">
+                        <figcaption><a href="paginas/quem_somos.php">Quem Somos</a></figcaption>
+                        </figure>
                     </section>
                 </div>
-                <div class="col-sm-12 col-md-12 col-lg-12">
+                <div class="col-sm-12 col-md-6 col-lg-6">
                 <section class = "padding">
+                        <figure>
                         <img src="https://placekitten.com/303/313" alt="Imagem Via Placekitten">
+                        <figcaption><a href="paginas/contato.php">Contato</a></figcaption>
+                        </figure>
+                    </section>
+                </div>
+                <div class="col-sm-12 col-md-6 col-lg-6">
+                <section class = "padding">
+                        <figure>
+                        <img src="https://placekitten.com/303/313" alt="Imagem Via Placekitten">
+                        <figcaption><a href="paginas/termos_de_uso.php">Termos de Uso</a></figcaption>
+                        </figure>
                     </section>
                 </div>
              
@@ -54,9 +83,7 @@ T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="a
             </div>
         </div>
 
-    <footer class="text-center">
-        <br><small>Copystart Joel Teixeira Junior - RA: 2650832313001 - Todos os Direitos estão Reservados.</small>
-    </footer>
+        <?php include_once 'includes/inc_footer_index.php'; ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-
 C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
