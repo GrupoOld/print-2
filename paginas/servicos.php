87dbf296e5ecc15d55d55511cf026f4d73528fa2
guilherme-filipini <139499115+Poser404@users.noreply.github.com>
2023-11-28 18:52:09 -0300

    Add files via upload

---
+++ paginas/servicos.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portal de São Roque: Serviços</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-
+T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/meu.css">
+</head>
+<body>
+    <div class = "container text-center">
+        <div class = "row">
+        <?php include_once '../includes/inc_header.php'; ?>
+            <div class="col-sm-12 col-md-12 col-lg-12">
+                <article>
+                    <h1>EM MANUTENÇÃO</h1>
+                    <img src="https://placekitten.com/1300/500" class="img-fluid" alt="Logotipo da empresa de nome Gold">
+                    <p>Devido à escassez de funcionários, a manutencão da página de serviços do Portal de São Roque tem enfrentado desafios. A equipe reduzida busca otimizar recursos, priorizando atualizações essenciais e respondendo a solicitações prioritárias da comunidade.</p>
+                    <p>A administração do portal está ciente da situação e procura alternativas para reforçar a equipe e garantir a eficiência na prestação de informações e serviços online.</p>
+                </article>
+            </div>
+
+        </div>
+
+    </div>
+    <?php include_once '../includes/inc_footer.php'; ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-
+C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
