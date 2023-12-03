9834feaf59b8ef47bc7dff02a2d2603c6a4914fb
lucia-nishimori <141664115+lucia-nishimori@users.noreply.github.com>
2023-12-03 20:04:07 -0300

    Add files via upload

--- includes/inc_headerluc.php
+++ includes/inc_headerluc.php
@@ -4,44 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../css/meu.css">
 </head>
  
- 
-  <header class="fundo-lucia">
-    <div class="container text-center d-flex justify-content-center">
-      <nav class="navbar navbar-expand-lg" style="background-color: purple;">
+<div class="container text-center">
+          <nav class="navbar navbar-expand-lg menu_tur d-lg-flex" style="background-color: #EAB4F0;">
           <div class="container-fluid">
-              <a class="navbar-brand" href="#">
-                <img src="../images/logo_cffa2.svg" alt="Logotipo das Criaturas Fofas e Fedorentas do Abismo" width="100" height="50">
-              </a>
+             
                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                   data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                   aria-expanded="false" aria-label="Toggle navigation">
                   <span class="navbar-toggler-icon"></span>
                </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+               
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                
                   <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                   <li class="nav-item ">
-                       <a class="nav-link active" href="../paginas/pontosturisticos.php">Home Turismo</a>
-                    </li>
-                    <li class="nav-item">
-                       <a class="nav-link active" href="../paginas/roteirodovinho.php">Roteiro do Vinho</a>
-                    </li>
-                    <li class="nav-item ">
-                      <a class="nav-link active" href="../paginas/castelo.php">Castelo</a>
-                    </li>
-                    <li class="nav-item ">
-                      <a class="nav-link active" href="../paginas/catarina.php">Catarina</a>
-                   </li>
+                  
+                     <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="pontosturisticos.php">Home</a>
+                      </li>
+                     <li class="nav-item nav_tur">
+                        <a class="nav-link active" aria-current="page" href="roteirodovinho.php">Roteiro do Vinho</a>
+                      </li>
+                      <li class="nav-item ">
+                       <a class="nav-link active" aria-current= "page" href="castelo.php">Castelo</a>
+                      </li>
+                      <li class="nav-item ">
+                        <a class="nav-link active" aria-current= "page" href="catarina.php">Catarina</a>
+                     </li>
                   </ul>
-              </div>
+                </div>
           </div>
-      </nav>
+          </nav>
     </div>
-    <div class="container text-center d-flex justify-content-center">
-        <div>
-                <h1 class=center-lucia>TURISMO</h1>
-        </div>
-    </div>
-  </header>
   
+   
