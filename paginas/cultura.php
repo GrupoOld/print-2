215ab794ce455b38ba1d599d925c93dbc0d8f543
Joel Teixeira Junior <149331291+joel-t-junior@users.noreply.github.com>
2023-12-03 17:14:54 -0300

    Edição final

--- paginas/cultura.php
+++ paginas/cultura.php
@@ -12,113 +12,117 @@ T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="a
 </head>
 
 <body>
-    <!-- <?php include_once 'includes/inc_header.php'; ?> -->
     <header class="header_cl">
         <div class="container text-center">
-            <menu class="navbar navbar-expand-lg" style="background-color: yellow;">
-                <div class="container-fluid">
-                <a class="navbar-brand" href="culturaelazer.php">
-                        <img src="../images/logo_cffa2.svg" alt="Logotipo Criaturas Fofas e Fedorentas do Abismo" width="100" height="100">
+            <div class="row">
+                <div class="col-1">
+                    <a class="logocompadding" href="../index.php">
+                        <img src="../images/logo_cffa2.svg" alt="Logotipo das Criaturas Fofas e Fedorentas do Abismo"
+                            width="100" height="100">
                     </a>
+                </div>
+                <div class="col-11">
+                    <h1 class="titulo_cl">CULTURA</h1>
+                </div>
+            </div>
+        </div>
+        <?php include_once '../includes/inc_header.php'; ?>
+        <div class="container text-center">
+            <menu class="navbar navbar-expand-lg menu_cl" style="background-color: yellow;">
+                <div class="container-fluid">
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                         data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                         aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0 cl">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="cultura.php">Cultura</a>
+                                <a class="nav-link active" aria-current="page" href="cultura.php">CULTURA</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="lazer.php">Lazer</a>
+                                <a class="nav-link active" aria-current="page" href="lazer.php">LAZER</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="agenda.php">Agenda</a>
+                                <a class="nav-link active" aria-current="page" href="agenda.php">AGENDA</a>
                             </li>
                         </ul>
                     </div>
                 </div>
             </menu>
-            <div class="row">
-                <h1 class="titulo_cl">CULTURA</h1>
-            </div>
-        </div>
-        </div>
+        </div>        
     </header>
     <article>
         <div class="container text-center">
             <div class="row-fluid">
                 <div class="col-sm-12 col-md-12 col-lg-12">
                     <br>
-                    <img src="https://placekitten.com/400/200" class="img-fluid" alt="Imagem de Capa da Cultura">
+                    <img src="../images/capacultura.jpg" class="img-fluid"
+                        alt="Imagem de Capa da Cultura, um bilhete antigo">
                 </div>
             </div>
         </div>
-        <div class="container text-center">
+        <div class="container text-center cl">
             <div class="col-sm-12 col-md-12 col-lg-12"></div>
             <h3><br>CULTURA DE SÃO ROQUE</h3><br>
         </div>
-        <div class="container">
+        <div class="container cl">
             <div class="row">
-                <div class="col-sm-12 col-md-6 col-lg-6">
+                <div class="col-sm-12 col-md-12 col-lg-12">
                     <p>Bem vindos à divisão de Cultura da Prefeitura do Município de São Roque!</p>
-                    <p>Aqui encontrará notícias e agenda para não se perder no calendário.</p>
-                    <img src="https://placekitten.com/400/200" class="img-fluid" alt="Logotipo da empresa de nome Gold">
+                    <p>A Divisão de Cultura da cidade de São Roque, no interior de São Paulo, é um órgão da Prefeitura
+                        Municipal responsável por promover e fomentar as atividades culturais no município. A Divisão é
+                        composta por uma equipe de profissionais qualificados, que atuam nas áreas de música, dança,
+                        teatro, cinema, artes visuais, literatura, patrimônio cultural e educação cultural.</p>
+                    <img src="../images/10conferencia.jpg" class="mx-auto d-block"
+                        alt="Arte sobre a décima conferência sobre Cultura">
                 </div>
-                <div class="col-md-6 col-lg-6">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis possimus ducimus esse quia
-                        pariatur, non iste voluptatem laborum alias deleniti repellendus eveniet voluptatibus velit. Nam
-                        ea quam alias dolor unde.</p>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto laborum officia dolor
-                        consequuntur quod aliquid mollitia ipsum facilis neque tempora, tenetur numquam, qui cumque
-                        voluptas perspiciatis minus ipsa. Quaerat, non!</p>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus beatae expedita laborum,
-                        adipisci cumque illum? Deleniti, provident? Ex, consequuntur eos? Tempore fuga ea ipsum iste
-                        accusantium placeat cum possimus aperiam.</p>
-                    <img src="https://placekitten.com/405/200" class="img-fluid" alt="Logotipo da empresa de nome Gold">
+                <div class="col-sm-12 col-md-6 col-lg-6">
+                    <p>A Divisão Municipal de Cultura da Prefeitura da Estância Turística de São Roque convida todo o
+                        setor cultural da cidade a participar da 10º Conferência Municipal de Cultura, que ocorrerá de
+                        forma presencial, nos dias 23 e 24 de outubro, das 18h às 22h horas, no salão dos Italianos, no
+                        CECT Brasital.</p>
+                </div>
+                <div class="col-sm-12 col-md-6 col-lg-6">
+                    <p>O objetivo da Conferência Municipal de Cultura é garantir a ampla participação popular na
+                        construção das Políticas Públicas Culturais do município de São Roque.</p>
                 </div>
             </div>
         </div>
     </article>
-    <footer class="text-center">
-
-
-    <div class="container">
-        <div class="row">
-
-            <section class="col-1 col-md-1 align-self-start">
-                <h1><a href="www.instagram.com.br"><span class="bi bi-instagram"></span></a></h1>
-            </section>
-            <section class="col-1 col-md-1 align-self-start">
-                <h1><a href="www.youtube.com.br"><span class="bi bi-youtube"></span></a></h1>
-            </section>
-            <section class="col-1 col-md-1 align-self-star">
-                <h1><a href="www.twitter.com.br"><span class="bi bi-twitter"></span></a></h1>
-            </section>
-            <section class="col-1 col-md-1 align-self-start">
-                <h1><a href="www.facebook.com.br"><span class="bi bi-facebook"></span></a></h1>
-            </section>
-
-            <div class="col-5 nomes">
-                <small>Todos Direitos Reservados:</small>
-                <br><small>Gleiton Correia Fernandes - RA: 2650832313036 </small>
-                <br><small>Guilherme Filipini - RA: 2650832313008 -</small>
-                <br><small>Joel Teixeira Jr - RA: 2650832313001 </small>
-                <br><small>Lucia Midori Nishimori - RA: 2650832313035</small>
-            </div>
-
-
-            <div class="col-3 logof">
-                <a class="navbar-brand" href="#">
-                    <img src="../images/logo_cffa2.svg" alt="Logo GOLD" width="100" height="100">
-                </a>
+    <footer class="text-center footer_cl">
+        <div class="container">
+            <div class="row">
+                <section class="col-6 col-sm-6 col-md-1 col-lg-1">
+                    <h1><a href="www.instagram.com.br"><span class="bi bi-instagram"></span></a></h1>
+                </section>
+                <section class="col-6 col-sm-6 col-md-1 col-lg-1">
+                    <h1><a href="www.youtube.com.br"><span class="bi bi-youtube"></span></a></h1>
+                </section>
+                <section class="col-6 col-sm-6 col-md-1 col-lg-1">
+                    <h1><a href="www.twitter.com.br"><span class="bi bi-twitter"></span></a></h1>
+                </section>
+                <section class="col-6 col-sm-6 col-md-1 col-lg-1">
+                    <h1><a href="www.facebook.com.br"><span class="bi bi-facebook"></span></a></h1>
+                </section>
+                <div class="col-8 col-sm-7 col-md-6 col-lg-6">
+                    <small>Todos Direitos Reservados:</small>
+                    <br><small>Gleiton Correia Fernandes - RA: 2650832313036</small>
+                    <br><small>Guilherme Filipini - RA: 2650832313008</small>
+                    <br><small>Joel Teixeira Jr - RA: 2650832313001</small>
+                    <br><small>Lucia Midori Nishimori - RA: 2650832313035</small>
+                </div>
+                <div class="col-4 col-sm-5 col-md-2 col-lg-2">
+                    <div class="logocompadding">
+                        <a class="navbar-brand" href="../index.php">
+                            <img src="../images/logo_cffa2.svg"
+                                alt="Logotipo das Criaturas Fofas e Fedorentas do Abismo" width="100" height="100">
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-
-</footer>
-
+    </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-
 C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
