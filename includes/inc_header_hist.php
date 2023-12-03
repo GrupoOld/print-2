7645fd1fbed5857fa6235da4b74e880f96249f75
guilherme-filipini <139499115+Poser404@users.noreply.github.com>
2023-12-03 17:17:26 -0300

    Add files via upload

---
+++ includes/inc_header_hist.php
@@ -0,0 +1,137 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Portal de São Roque: História Geral</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-
+T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/gui.css">
+</head>
+
+<body>
+    <header>
+<div class="header_hist">
+            <div class="container text-center">
+                <div class="row">
+                    <div class="col-1">
+                        <a class="logocompadding" href="../index.php">
+                            <img src="../images/logo_cffa2.svg" alt="Logotipo das Criaturas Fofas e Fedorentas do Abismo" width="100" height="100">
+                        </a>
+                    </div>
+                    <div class="col-11">
+                        <h1 class="titulo_hist">História</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="container text-center">
+                <menu class="navbar navbar-expand-lg navbar-primary-hist">
+                    <div class="container-fluid">
+
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        História
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="../paginas/historia_geral.php">História Geral</a></li>
+                                        <li><a class="dropdown-item" href="../paginas/capela.php">Capela Santo Antônio</a></li>
+                                        <li><a class="dropdown-item" href="../paginas/brasital.php">Brasital</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Turismo
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="../paginas/pontosturisticos.php">Pontos Turísticos</a></li>
+                                        <li><a class="dropdown-item" href="../paginas/roteirodovinho.php">Roteiro do Vinho</a></li>
+                                        <li><a class="dropdown-item" href="../paginas/castelo.php">Castelo de São Roque</a></li>
+                                        <li><a class="dropdown-item" href="../paginas/catarina.php">Catarina Outlet</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="../paginas/serviços.php">Serviços</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="../paginas/home_noticias.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Notícias
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="../paginas/home_noticias.php">História Geral</a></li>
+                                        <li><a class="dropdown-item" href="../paginas/noticias2.php">Notícias da Cidade</a></li>
+                                        <li><a class="dropdown-item" href="../paginas/noticias3.php">Notícias da Cultura</a></li>
+                                        <li><a class="dropdown-item" href="../paginas/noticias4.php">Notícias da Educação</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="../paginas/culturaelazer.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Cultura e Lazer
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="../paginas/culturaelazer.php">Home - Cultura e Lazer</a></li>
+                                        <li><a class="dropdown-item" href="../paginas/cultura.php">Cultura</a></li>
+                                        <li><a class="dropdown-item" href="../paginas/lazer.php">Lazer</a></li>
+                                        <li><a class="dropdown-item" href="../paginas/agenda.php">Agenda</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="../paginas/quem_somos.php">Quem Somos</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="../paginas/contato.php">Contato</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="../paginas/termos_de_uso.php">Termos de Uso</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </menu>
+            </div>
+            <div class="headerhomehist">
+            <div class="container text-center">
+                <menu class="navbar navbar-expand-lg navbar2-hist">
+                    <div class="container-fluid">
+
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="historia_geral.php">História Geral</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="capela.php">Capela de Santo Antônio</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="brasital.php">Brasital</a>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </menu>
+            </div>
+        </div>
+        </div>
+        </div>
+
+
+
+        
+</header>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-
+C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
