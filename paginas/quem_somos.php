4f527000e8e87dc41c93da08b8bf74b79e8803ed
GleitonF <149203589+GleitonF@users.noreply.github.com>
2023-12-03 15:03:43 -0300

    pagina quem somos
    Guilherme só altera o miolo dela com nosssas informações e fotos, mantenha o css, footer e header

--- paginas/quem_somos.php
+++ paginas/quem_somos.php
@@ -4,19 +4,20 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Quem Somos?</title>
+    <title>Home noticias</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-
 T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/meu.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 </head>
 
 <body>
     <?php include_once '../includes/inc_header.php'; ?>
-    <div class = "container text-center">
-        <div class = "row">
+    <div class="container text-center">
+        <div class="row">
             <div class="col-md-12 col-lg-12">
                 <article>
-                <img src="https://placekitten.com/1195/707" class="img-fluid" alt="Logotipo da empresa de nome Gold">
+                    <img src="https://placekitten.com/1195/707" class="img-fluid" alt="Logotipo da empresa de nome Gold">
                     <p>Cidade - LOREM - "Não há ninguém que ame a dor por si só, que a busque e queira tê-la, simplesmente por ser dor..."Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed quam rutrum, placerat lectus nec, vestibulum lectus. Curabitur a vehicula orci, quis maximus ipsum. Quisque vitae nisl nec elit convallis pretium. Vivamus euismod justo non felis condimentum ultrices ut vel mauris. Duis justo est, vestibulum sed consectetur et, ultricies nec magna. Donec ex dolor, condimentum id hendrerit et, venenatis eleifend orci. Nulla ultrices sollicitudin tellus, sit amet mattis diam tempus nec. Phasellus iaculis aliquam orci. Nulla vel facilisis lectus, sit amet elementum odio. Aliquam tellus orci, tempor eget tempus eu, pulvinar at erat. Vivamus et euismod urna, non porta sem.</p>
                     <p>Nunc vel bibendum velit, eu tempor est. Nulla congue massa faucibus justo sollicitudin tincidunt vel sit amet tellus. In ultrices libero sit amet dui accumsan scelerisque. Vestibulum auctor, eros nec aliquet sollicitudin, elit ipsum condimentum nulla, vitae mattis mauris ligula sed ligula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc pharetra ornare eros, a luctus ligula viverra eu. Proin mi velit, ultrices nec dui a, auctor cursus leo. Sed porta nunc nulla, ut dapibus quam laoreet malesuada. Vivamus laoreet ligula metus, ut vehicula nulla semper ut. Donec est nisi, dictum sed ligula eget, venenatis eleifend nulla.</p>
                 </article>
@@ -24,9 +25,51 @@ T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="a
         </div>
 
     </div>
-    <footer class="text-center">
-        <br><small>Copystart Joel Teixeira Junior - RA: 2650832313001 - Todos os Direitos estão Reservados.</small>
+
+    <footer class="text-center mx-auto">
+
+        <div class="container">
+            <div class="row">
+
+                <section class="col-1 col-md-1 align-self-start">
+                    <h1><a href="https://www.instagram.com.br" target="_blank"><span class="bi bi-instagram" style="color: red;"></span></a></h1>
+                </section>
+                <section class="col-1 col-md-1 align-self-star">
+                    <h1><a href="https://www.twitter.com.br" target="_blank"><span class="bi bi-twitter" style="color: blue;"></span></a></h1>
+                </section>
+                <section class="col-1 col-md-1 align-self-start">
+                    <h1><a href="https://www.youtube.com.br" target="_blank"><span class="bi bi-youtube" style="color: red;"></span></a></h1>
+                </section>
+                <section class="col-1 col-md-1 align-self-start">
+                    <h1><a href="https://www.facebook.com.br" target="_blank"><span class="bi bi-facebook" style="color: blue;"></span></a></h1>
+                </section>
+
+                <div class="col-5 nomes">
+                    <small>Todos Direitos Reservados:</small>
+                    <br><small>Gleiton Correia Fernandes - RA: 2650832313036 </small>
+                    <br><small>Guilherme Filipini - RA: 2650832313008 -</small>
+                    <br><small>Joel Teixeira Jr - RA: 2650832313001 </small>
+                    <br><small>Lucia Midori Nishimori - RA: 2650832313035</small>
+                </div>
+
+
+                <div class="col-3 logof">
+                    <a class="navbar-brand" href="../paginas/index.php">
+                        <img src="../images/logo_cffa2.svg" alt="Logo GOLD" width="100" height="100">
+                    </a>
+                </div>
+            </div>
+        </div>
+
     </footer>
+
 </body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-
+        C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.7/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 
 </html>
