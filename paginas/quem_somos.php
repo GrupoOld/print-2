90cfcf51f803626ae36041acad23250f3b3a9016
guilherme-filipini <139499115+Poser404@users.noreply.github.com>
2023-12-03 15:12:36 -0300

    Update quem_somos.php

--- paginas/quem_somos.php
+++ paginas/quem_somos.php
@@ -13,63 +13,114 @@ T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="a
 
 <body>
     <?php include_once '../includes/inc_header.php'; ?>
-    <div class="container text-center">
-        <div class="row">
-            <div class="col-md-12 col-lg-12">
-                <article>
-                    <img src="https://placekitten.com/1195/707" class="img-fluid" alt="Logotipo da empresa de nome Gold">
-                    <p>Cidade - LOREM - "Não há ninguém que ame a dor por si só, que a busque e queira tê-la, simplesmente por ser dor..."Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed quam rutrum, placerat lectus nec, vestibulum lectus. Curabitur a vehicula orci, quis maximus ipsum. Quisque vitae nisl nec elit convallis pretium. Vivamus euismod justo non felis condimentum ultrices ut vel mauris. Duis justo est, vestibulum sed consectetur et, ultricies nec magna. Donec ex dolor, condimentum id hendrerit et, venenatis eleifend orci. Nulla ultrices sollicitudin tellus, sit amet mattis diam tempus nec. Phasellus iaculis aliquam orci. Nulla vel facilisis lectus, sit amet elementum odio. Aliquam tellus orci, tempor eget tempus eu, pulvinar at erat. Vivamus et euismod urna, non porta sem.</p>
-                    <p>Nunc vel bibendum velit, eu tempor est. Nulla congue massa faucibus justo sollicitudin tincidunt vel sit amet tellus. In ultrices libero sit amet dui accumsan scelerisque. Vestibulum auctor, eros nec aliquet sollicitudin, elit ipsum condimentum nulla, vitae mattis mauris ligula sed ligula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc pharetra ornare eros, a luctus ligula viverra eu. Proin mi velit, ultrices nec dui a, auctor cursus leo. Sed porta nunc nulla, ut dapibus quam laoreet malesuada. Vivamus laoreet ligula metus, ut vehicula nulla semper ut. Donec est nisi, dictum sed ligula eget, venenatis eleifend nulla.</p>
-                </article>
+    <div class = "container">
+        <div class = "row">
+            <div class = "container text-center">
+                <section>
+                    <h2>Quem Somos?</h2>
+                </section>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-6">
+        <article>
+            <img src="../images/gleiton_photo.jpg" class = "img-fluid" alt="Gleiton Fernandes">
+            <table>
+                <tr>
+                    <th>Gleiton Correa Fernandes</th>
+                </tr>
+                <tr>
+                    <td>RA: 2650832313034</td>
+                </tr>
+                <tr>
+                    <td>
+                        - Idealização do projeto; <br>- Layout e produção do Site; <br>- MockUp e paleta de cores; <br>- Edição de Imagens; <br>- Texto <br>- Entrevista com Artista.
+                    </td>
+                </tr>                
+                <tr>
+                    <td>
+                        Nascido em 1986 em São Roque SP, desde criança foi apoxonado por games e tecnologia, sempre tive curiosidade de saber como funcionava o mundo da programação. Sempre trabalhei com lógistica e em 2022 resolvi mudar de área assim então prestando vestibular na Fatec SR com objetivo de conhecer e aprender programação.
+                    </td>
+                </tr>                   
+            </table>
+        </article>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-6">
+        <article>
+            <img src="../images/luciaphoto.jpg" class = "img-fluid" alt="Lucia Midori">
+            <table>
+                <tr>
+                    <th>Lucia Midori Nishimori</th>
+                </tr>
+                <tr>
+                    <td>RA: 2650832313035</td>
+                </tr>
+                <tr>
+                    <td>- Idealização do projeto; <br>- Layout do Projeto; <br>- Produção de código; <br>- Texto; <br>- MockUp;</td>
+                </tr>                
+                <tr>
+                    <td>
+                    Nascida na cidade de Mairinque, entrei na área de tecnolologia e trabalhei por alguns anos, resolvi adentrar nesse mundo novamente pela curiosidade das novas tecnologias que estão disponíveis no mercado.<br><br><br><br>
+                    </td>
+                </tr>                
+            </table>
+        </article>      
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-6">
+             <article>
+            <img src="../images/joelphoto.jpg" class = "img-fluid" alt="Joel Junior">
+            <table>
+                <tr>
+                    <th>Joel Teixeira Junior</th>
+                </tr>
+                <tr>
+                    <td>RA: 2650832313001</td>
+                </tr>
+                <tr>
+                    <td>- Produção de código; <br>- Idealização do projeto; <br>- Edição de quase todas as imagens; <br>- Escolhas das cores.</td>
+                </tr>                
+                <tr>
+                    <td>
+                        Nascido em 1983 na cidade de Carapicuíba - SP, Joel desde criança sempre foi curioso, desmontava brinquedos para ver como funcionavam, aos 15 anos comprava peças antigas e montava computadores para seus familiares, aos 17 anos assim que terminou o segundo grau, resolveu fazer um curso Técnico de Informática, chegou a passar em segundo lugar no vestibulinho mas o curso foi cancelado naquele ano. Hoje está em busca do tão desejado sonho de trabalhar com informática.
+                    </td>
+                </tr>                
+            </table>
+        </article> 
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-6">
+        <article>
+            <img src="../images/guilherme_photo.jpg" class = "img-fluid" alt="Guilherme Filipini">
+            <table>
+                <tr>
+                    <th>Guilherme Filipini de Albuquerque</th>
+                </tr>
+                <tr>
+                    <td>RA: 2650832313008</td>
+                </tr>
+                <tr>
+                    <td>- Produção de código; <br>- Idealização do projeto; <br>- Edição de imagem; <br>- Layout do site.</td>
+                </tr>                
+                <tr>
+                    <td>
+                    Guilherme Filipini de Albuquerque é nascido em Itapevi, SP, no dia 12/03/2005, e desde muito jovem tem interesse na tecnologia. Ele se destaca por sua dedicação e habilidades práticas em informática durante sua formação em informática. Guilherme agora está trabalhando para construir uma carreira promissora na tecnologia, observando novas tendências e criando novas soluções.<br><br>
+                    </td>
+                </tr>                
+            </table>
+        </article>       
+        </div>
+        <div class = 'container text-center'>
+        <div class="col-sm-12 col-md-12 col-lg-12">
+            <section class = "h3-link">
+                <a href="../paginas/contato.php"><h3>Entre em Contato!</h3></a>
+            </section>
             </div>
         </div>
-
-    </div>
-
-    <footer class="text-center mx-auto">
-
-        <div class="container">
-            <div class="row">
-
-                <section class="col-1 col-md-1 align-self-start">
-                    <h1><a href="https://www.instagram.com.br" target="_blank"><span class="bi bi-instagram" style="color: red;"></span></a></h1>
-                </section>
-                <section class="col-1 col-md-1 align-self-star">
-                    <h1><a href="https://www.twitter.com.br" target="_blank"><span class="bi bi-twitter" style="color: blue;"></span></a></h1>
-                </section>
-                <section class="col-1 col-md-1 align-self-start">
-                    <h1><a href="https://www.youtube.com.br" target="_blank"><span class="bi bi-youtube" style="color: red;"></span></a></h1>
-                </section>
-                <section class="col-1 col-md-1 align-self-start">
-                    <h1><a href="https://www.facebook.com.br" target="_blank"><span class="bi bi-facebook" style="color: blue;"></span></a></h1>
-                </section>
-
-                <div class="col-5 nomes">
-                    <small>Todos Direitos Reservados:</small>
-                    <br><small>Gleiton Correia Fernandes - RA: 2650832313036 </small>
-                    <br><small>Guilherme Filipini - RA: 2650832313008 -</small>
-                    <br><small>Joel Teixeira Jr - RA: 2650832313001 </small>
-                    <br><small>Lucia Midori Nishimori - RA: 2650832313035</small>
-                </div>
-
-
-                <div class="col-3 logof">
-                    <a class="navbar-brand" href="../paginas/index.php">
-                        <img src="../images/logo_cffa2.svg" alt="Logo GOLD" width="100" height="100">
-                    </a>
-                </div>
-            </div>
         </div>
-
-    </footer>
-
-</body>
-
+        </div>
+    <?php include_once "../includes/inc_footer.php"; ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-
         C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.7/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
+</body>
 
 </html>
